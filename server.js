bb1b8ab0920ca4fa7c81c73da9218e772d261e30
SoullessSimon <99135618+SoullessSimon@users.noreply.github.com>
2022-02-06 22:06:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,60 @@
+﻿const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Yo boi!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+const { Client, RichEmbed } = require("discord.js");
+const { CommandHandler } = require("djs-commands");
+const client = new Client({ disableEveryone: true });
+var approx = require('approximate-number');
+const config = require("./config.json");
+const CH = new CommandHandler({
+    folder: __dirname + "/Commands/",
+    prefix: config.prefix 
+});
+
+
+client.on("ready", () => {
+    console.log("Xenon V2 XD");
+    client.user.setActivity(`!help`, {type: "LISTENING"})
+});
+
+client.on("message", async (message) => {
+
+    if(message.author.type === 'bot') return;
+    let args = message.content.split(" ");
+    let command = args[0];
+    let cmd = CH.getCommand(command);
+    if(!cmd) return;
+
+    try{
+        cmd.run(client,message,args)
+    }catch(e){
+        console.log(e)
+    }
+
+});
+
+let info = client.emojis.get("655091815401127966") || "ℹ️" //https://cdn.discordapp.com/emojis/655091815401127966.png?v=1
+
+client.on("guildCreate", guild => {
+  
+      let channelID;
+    let channels = guild.channels;
+    channelLoop:
+    for (let c of channels) {
+        let channelType = c[1].type;
+        if (channelType === "text") {
+            channelID = c[0];
+            break channelLoop;
+        }
+    }
+
+    
+})
+
+client.login(config.token)
